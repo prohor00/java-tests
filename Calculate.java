b6262aaca2a94844a068ed80d3ebda9184b2237c
Prohor00 <prohorovdmitrij@yandex.ru>
2019-02-25 16:05:07 +0300

    change Calculate

--- Calculate.java
+++ Calculate.java
@@ -1,5 +1,15 @@
 public class Calculate {
 	public static void main(String[] arg) {
 		System.out.println("Calculate...");
+		int first = Integer.valueOf(arg[0]);
+		int second = Integer.valueOf(arg[1]);
+		int sum = first + second;
+		int minus = first - second;
+		int multiply = first * second;
+		int share = first / second;
+		System.out.println("sum = " + sum);
+		System.out.println("minus = " + minus);
+		System.out.println("multiply = " + multiply);
+		System.out.println("share = " + share);
 	}	
 }
